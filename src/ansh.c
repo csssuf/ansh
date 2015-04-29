ccfb9abf4fe4c43b39746faac7cb5e53388daa7a
James Forcier <csssuf@csssuf.net>
2015-04-28 22:54:22 -0400

    Fix unused warning

--- src/ansh.c
+++ src/ansh.c
@@ -4,10 +4,13 @@
 
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <octo/cll.h>
 
 #include "mpc.h"
 #include "ansh/ansh.h"
 
 int main(int argc, char* argv[]) {
+    if(argc == 1 || argv == NULL) {}
     return EXIT_SUCCESS;
 }
+
