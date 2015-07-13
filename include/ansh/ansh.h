93c7fed2a39324befdbe5a5b9a5b51301866d408
James Forcier <csssuf@csssuf.net>
2015-07-12 21:40:17 -0400

    Added environment variable hashtable

--- include/ansh/ansh.h
+++ include/ansh/ansh.h
@@ -1,4 +1,16 @@
 #ifndef ANSH_ANSH_H
 #define ANSH_ANSH_H
 
+#define ENV_INIT_OCTO_KEY_SIZE	32
+#define ENV_INIT_OCTO_VAL_SIZE	256
+#define ENV_INIT_OCTO_BUCKETS	16
+
+#include <octo/cll.h>
+
+typedef struct hashable_str_s {
+	char str[ENV_INIT_OCTO_KEY_SIZE];
+} hashable_str;
+
+octo_dict_cll_t *setup();
+
 #endif //ANSH_ANSH_H
