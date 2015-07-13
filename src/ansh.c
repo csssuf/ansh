da4a9a625f9ec3c97f009471195cb09dbdfc7224
James Forcier <csssuf@csssuf.net>
2015-07-12 21:53:18 -0400

    Convert tabs to spaces

--- src/ansh.c
+++ src/ansh.c
@@ -12,32 +12,32 @@
 
 int main(int argc, char* argv[]) {
     if(argc == 1 || argv == NULL) {}
-	octo_dict_cll_t *env_vars = setup();
-	hashable_str *key_prompt = malloc(sizeof(hashable_str));
-	memset(key_prompt, 0, sizeof(hashable_str));
-	strcpy(key_prompt->str, "PROMPT");
-	fprintf(stderr, "PROMPT: %s", (char *)(octo_cll_fetch(key_prompt, env_vars)));
+    octo_dict_cll_t *env_vars = setup();
+    hashable_str *key_prompt = malloc(sizeof(hashable_str));
+    memset(key_prompt, 0, sizeof(hashable_str));
+    strcpy(key_prompt->str, "PROMPT");
+    fprintf(stderr, "PROMPT: %s", (char *)(octo_cll_fetch(key_prompt, env_vars)));
     return EXIT_SUCCESS;
 }
 
 octo_dict_cll_t *setup() {
-	uint8_t *master_key = octo_keygen();
-	octo_dict_cll_t *vars = octo_cll_init(
-		ENV_INIT_OCTO_KEY_SIZE,
-		ENV_INIT_OCTO_VAL_SIZE,
-		ENV_INIT_OCTO_BUCKETS,
-		master_key
-	);
+    uint8_t *master_key = octo_keygen();
+    octo_dict_cll_t *vars = octo_cll_init(
+        ENV_INIT_OCTO_KEY_SIZE,
+        ENV_INIT_OCTO_VAL_SIZE,
+        ENV_INIT_OCTO_BUCKETS,
+        master_key
+    );
 
-	hashable_str *key_prompt = malloc(sizeof(hashable_str));
-	memset(key_prompt, 0, sizeof(hashable_str));
-	strcpy(key_prompt->str, "PROMPT");
-	hashable_str *val_prompt = malloc(sizeof(hashable_str));
-	memset(val_prompt, 0, sizeof(hashable_str));
-	strcpy(val_prompt->str, "ansh $ ");
-	if(octo_cll_insert(key_prompt, val_prompt, vars) != 0) {
-		fprintf(stderr, "Error adding PROMPT variable during setup()\n");
-		return NULL;
-	}
-	return vars;
+    hashable_str *key_prompt = malloc(sizeof(hashable_str));
+    memset(key_prompt, 0, sizeof(hashable_str));
+    strcpy(key_prompt->str, "PROMPT");
+    hashable_str *val_prompt = malloc(sizeof(hashable_str));
+    memset(val_prompt, 0, sizeof(hashable_str));
+    strcpy(val_prompt->str, "ansh $ ");
+    if(octo_cll_insert(key_prompt, val_prompt, vars) != 0) {
+        fprintf(stderr, "Error adding PROMPT variable during setup()\n");
+        return NULL;
+    }
+    return vars;
 }
